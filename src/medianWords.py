aa2034dbde0cdfa2f756fae3e8362a316a3d7ecb
Charles Showley <cgshowley@gmail.com>
2015-07-20 01:58:55 -0700

    Coding Challenge v. 3

--- src/medianWords.py
+++ src/medianWords.py
@@ -1,10 +1,18 @@
 from os.path import join
-import time
+from sortedcontainers import SortedList
 
-t1 = time.time()
+# Calculate the median based on the previously sorted list of words
+def calculateMedian(words, numWords):
+    if numWords%2 == 0:
+        mid = numWords/2
+        median = ((words[mid]+words[mid-1])/2.0)
+    else:
+        mid = (numWords-1)/2
+        median = words[mid]
+    return median
 
 # Read raw tweet data into script, this time preserving each full tweet rather than parse each word as was done in numWords.py
-filename = 'newData.txt'
+filename = 'tweets.txt'
 i = 0
 with open(join('./tweet_input/', filename),'rU') as data:
     tweets = data.readlines()
@@ -12,94 +20,29 @@ with open(join('./tweet_input/', filename),'rU') as data:
         tweets[i] = line.strip('\n')
         i += 1
 
-
 # Create output file ft2.txt
-filename1 = 'myway.txt'
-filename2 = 'sort.txt'
-with open(join('./tweet_output/', filename1), 'w') as data:
-    pass
-with open(join('./tweet_output/', filename2), 'w') as data:
-    pass
-
-uWords1 = [] # Number of unique words per tweet, will increase until its length is equal to the length of 'tweets' list
-uWords2 = []
-for tweet in tweets:
-    tweet = tweet.split()
-    uniqueCount = {}    # Create a new dictionary for each tweet and overwrite the dictionary from the previous iteration of the loop. Each dictionary key is a unique word from the current tweet with a null value
-    for word in tweet:
-        if uniqueCount.has_key(word):
-            pass
-        else:
-            uniqueCount[word] = 1
-    foo = float(len(uniqueCount))
-
-    # --------- MAKE ME A FUNCTION --------------
-    # The list 'uWords' can be simply rearranged by uWords.sort(), but I've found that having to do that for very large numbers of tweets takes too much processing time. Given a value of unique words in a tweet, this function compares that value to the first, last, and middle indices in uWords and places it accordingly.
-
-    if not uWords1 == []:
-        midP = len(uWords1)/2
-        if foo == uWords1[midP]:
-            uWords1.insert(midP, foo)
-        elif foo >= uWords1[-1]:
-            uWords1.append(foo)
-        elif foo <= uWords1[0]:
-            uWords1.insert(0, foo)
-        elif foo > uWords1[midP]:
-            while foo > uWords1[midP]:
-                midP += 1
-            uWords1.insert((midP), foo)
-        elif foo < uWords1[midP]:
-            while foo < uWords1[midP]:
-                midP -= 1
-            uWords1.insert((midP+1), foo)
-    else:
-        uWords1.append(foo)
-
-
-    # -------------------------------------------
-
-    # ----- MAKE ME A FUNCTION ------------------
-    # Calculates the median
-    if len(uWords1)%2 == 0:
-        mid = len(uWords1)/2
-        median = ((uWords1[mid]+uWords1[mid-1])/2.0)
-
-    elif len(uWords1) == 1:
-        median = uWords1[0]
-    else:
-        mid = (len(uWords1)-1)/2
-        median = uWords1[mid]
-
-    # -------------------------------------------
-
-    with open(join('./tweet_output/', filename1), 'a') as data:
-        data.write('%s\n' % median)
-# ----------- # ------------- # ------------ # ------------ #
-
-    uniqueCount = {}
-
-    for word in tweet:
-        if uniqueCount.has_key(word):
-            pass
-        else:
-            uniqueCount[word] = 1
-    foo = float(len(uniqueCount))
-
-    uWords2.append(foo)
-    uWords2.sort()
-    midP = len(uWords2)/2
-
-    if len(uWords2)%2 == 0:
-        mid = len(uWords2)/2
-        median = ((uWords2[midP]+uWords2[mid-1])/2.0)
-    elif len(uWords2) == 1:
-        median = uWords2[0]
-    else:
-        mid = (len(uWords2)-1)/2
-        median = uWords2[mid]
-
-    with open(join('./tweet_output/', filename2), 'a') as data:
-        data.write('%s\n' % median)
+filename = 'ft2.txt'
+uniqueCount = {}
+uCount = uniqueCount.has_key
+with open(join('./tweet_output/', filename), 'w') as data:
     
+    uWords = SortedList([])            # Number of unique words per tweet, will increase until its length is equal to the length of 'tweets' list
+    uWords_add = uWords.add
+    for tweet in tweets:
+    
+        tweet = tweet.split()
+        
+        uniqueCount = {}    # Create a new dictionary for each tweet and overwrite the dictionary from the previous iteration of the loop. Each dictionary key is a unique word from the current tweet with a null value
+        for word in tweet:
+            if uCount(word):
+                pass
+            else:
+                uniqueCount[word] = 1
+        foo = float(len(uniqueCount))
+        
+        uWords_add(foo)
+        numWords = len(uWords)
+        median = calculateMedian(uWords, numWords)
 
-print time.time() - t1
+        data.write('%s\n' % median)
+
--- src/numWords.py
+++ src/numWords.py
@@ -1,57 +1,38 @@
 from os.path import join
 from re import search
-import time
 
-t1 = time.time()
+def repeatWords(words):
+    uWords = []     # A list to remove any repeated words in the list 'words'
+    wordCount = {}  # Dictionary keys are words appearing in tweets.txt and the value of each key is the number of times that specific word has appeared
+    for word in words:
+        if wordCount.has_key(word):
+            wordCount[word] += 1
+        else:
+            wordCount[word] = 1
+            uWords.append(word)
+    uWords.sort()
+    return uWords, wordCount
+
 # Read raw tweet data into script for processing
 
 filename = 'tweets.txt'
 words = [] # A list containing all tweets where each element is a word from a tweet
-#numChars = 0
-with open(join('../tweet_input/', filename),'rU') as data:
+with open(join('./tweet_input/', filename),'rU') as data:
     tweets = data.readlines()
     for line in tweets:
         word = line.split()
         for foo in word:
             # Python has trouble handling apostrophes (') and quotes (") which by default are read into the 'words' array as shown in the 'search' function. This causes problems when sorting words into ASCII order so all instances of (') and (") are manually replaced here
-            # ------- MAKE ME A FUNCTION -------------------
             if search('\xe2\x80\x98',foo):
                 foo = foo.replace('\xe2\x80\x98','\'')
             elif search('\xe2\x80\x9c',foo):
                 foo = foo.replace('\xe2\x80\x9c','"')
-            # ----------------------------------------------
             words.append(foo)
-            #if len(foo) > numChars:
-                #numChars = len(foo)
 
-# -------------- MAKE ME A FUNCTION ----------------------
-uWords = []     # A list to remove any repeated words in the list 'words'
-wordCount = {}  # Dictionary keys are words appearing in tweets.txt and the value of each key is the number of times that specific word has appeared
-for word in words:
-    if wordCount.has_key(word):
-        wordCount[word] += 1
-    else:
-        wordCount[word] = 1
-        uWords.append(word)
 
-uWords.sort()
-# --------------------------------------------------------
-
-#temp1 = []
-#temp2 = []
+uWords, wordCount = repeatWords(words)
 
 filename = 'ft1.txt'
-#template = '{0:%i} {1:10}\n' % numChars
-with open(join('../tweet_output/', filename),'w') as data:       # Create empty ft1.txt output file
-    pass
-for i in range(len(uWords)):
-    with open(join('../tweet_output/', filename),'a') as data:   # Append results to ft1.txt
+with open(join('./tweet_output/', filename),'w') as data:       # Create output file ft1.txt
+    for i in range(len(uWords)):
         data.write('%s' % uWords[i] +'\t'+ '%s\n' % wordCount[uWords[i]])
-        #data.write(template.format(short[i],wordCount[short[i]]))
-        #temp1.append(short[i])
-        #temp2.append(wordCount[short[i]])
-
-print time.time() - t1
-
-#print max(temp2)
-#print temp1[temp2.index(max(temp2))]
